b7fba5c3377774657c82c5e7abe26470c9ca286d
Gustavo Salgado <gustavo.salgado@bizapp.com.br>
2023-04-29 12:43:49 -0300

    teste res

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.json('Hello World!'))
+app.get('/', (req, res) => res.json('Hello World! - Após workflow'))
 app.listen(process.env.PORT || 3030)
