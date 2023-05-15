92ddcda5aabe6fbfbf34168766c8952d10ec1d26
Gustavo Salgado <gustavo.salgado@bizapp.com.br>
2023-05-15 20:57:48 -0300

    teste esteira

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(port, () => {console.log(`Listening at ${port}`)})
+app.listen(port, () => {console.log(`Listening at: ${port}`)})
