0aed92fe961f53f88316e06a3585d6f994349145
Gustavo Salgado <gustavo.salgado@bizapp.com.br>
2023-05-16 23:46:18 -0300

    add mapeamento perfil

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,6 @@
 const jwtController = require('../controller/jwtController');
 const gptController = require('../controller/gptController');
-
+const perfilController = require('../controller/perfilController');
 const express = require('express');
 const route = express.Router();
 
@@ -9,5 +9,8 @@ route.post('/api/v1/login', jwtController.login);
 
 //Rota API-GPT
 route.post('/api/v1/chat', gptController.getChat);
+
+//Rota Mapeamento Perfil
+route.post('/api/v1/mapeamento-perfil', perfilController.postPerfil);
 
 module.exports = route;
